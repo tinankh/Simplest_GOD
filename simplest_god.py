7fff42a48853ca366516c28d467e459ef2b38711
Tina NKH <tina.nikoukhah@gmail.com>
2019-07-01 18:13:19 +0200

    fast and slow all together

--- simplest_god.py
+++ simplest_god.py
@@ -1,68 +1,83 @@
-#
+#!/usr/bin/env python3
 
 #   SGOD - Simplest JPEG Grid Origin Detector
 #   Copyright (c) 2019 Tina Nikoukhah <tina.nikoukhah@gmail.com>
-
+#                      Jérémy Anger <angerj.dev@gmail.com>
+#
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU Affero General Public License as
 #   published by the Free Software Foundation, either version 3 of the
 #   License, or (at your option) any later version.
-
+#
 #   This program is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #   GNU Affero General Public License for more details.
-
+#
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#
-
-import argparse
-import os
-from PIL import Image
+import io
 import numpy as np
+from PIL import Image
 
-# Parse program arguments
-parser = argparse.ArgumentParser()
 
-parser.add_argument("input_filename", help="path to the input image")
-args = parser.parse_args()
+def get_jpeg_size(img, x, y):
+    width, height = img.size
+    width = (width // 8) * 8 - 8
+    height = (height // 8) * 8 - 8
+    crop_image = img.crop((x, y, width + x, height + y))
+    buffer = io.BytesIO()
+    crop_image.save(buffer, format='JPEG', quality=100)
+    return len(buffer.getvalue())
 
-img = Image.open(args.input_filename)
-width, height = img.size
+def detect_jpeg_grid(img, *, fast=False):
+    img = img.convert('YCbCr').split()[0]
+    get_size = lambda i, j: get_jpeg_size(img, i, j)
 
-width = int(width/8) * 8 - 8
-height = int(height/8) * 8 - 8
+    if not fast:
+        kappa = 7.0
+        coords = [(i,j) for i in range(8) for j in range(8)]
+        result = [get_size(i, j) for i, j in coords]
 
-ycbcr = img.convert('YCbCr')
-(Y, Cb, Cr) = ycbcr.split()
+        sorted_result = np.sort(result)
+        mean = np.mean(sorted_result[16:])
+        std = np.std(sorted_result[16:])
 
-filename = os.path.splitext(os.path.basename(str(args.input_filename)))[0]
+        mini = sorted_result[0]
 
-outdir="outdir_temp_"+str(filename)
+    else:
+        kappa = 7.0
+        coords = [(i,i) for i in range(8)]
+        result = [get_size(i, j) for i,j in coords]
 
-if not os.path.exists(outdir):
-    os.mkdir(outdir)
+        sorted_result = np.sort(result)
+        mean = np.mean(sorted_result[2:])
+        std = np.std(sorted_result[2:])
 
-result = []
+        i1 = np.where(result == sorted_result[0])[0][0]
+        i2 = np.where(result == sorted_result[1])[0][0]
+        mini = np.min([sorted_result[0], get_size(i1,i2), get_size(i2,i1)])
 
-for i in range(64):
-    x = i%8; y= int(i/8);
-    area = (x, y, width+x, height+y)
-    crop_image = Y.crop(area)
-    file_candidate = outdir + "/crop" + str(x) + "_" + str(y) + ".jpg"
-    crop_image.save(file_candidate, quality=100)
-    result.append(os.path.getsize(file_candidate))
+        for i in [(i1,i2), (i2,i1)]:
+            coords.append(i)
+            result.append(get_size(*i))
 
-aligned = np.sort(result)[16:]
+    if mini < (mean - kappa*std) :
+        xx, yy = coords[np.argmin(result)]
+        return xx,yy
 
-mean = np.mean(aligned)
-std = np.std(aligned)
+    else:
+        print("no grid found")
+        return -1,-1
 
-gridorigin = np.argmin(result)
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_filename", help="path to the input image")
+    parser.add_argument("--fast", action='store_true')
+    args = parser.parse_args()
 
-if np.min(result) < (mean - 7.0*std) :
-    print("grid origin: ({},{})".format(int(np.argmin(result)/8),np.argmin(result)%8))
-else:
-    print("no grid found")
+    img = Image.open(args.input_filename)
+    x, y = detect_jpeg_grid(img, fast=args.fast)
+    print(x, y)
